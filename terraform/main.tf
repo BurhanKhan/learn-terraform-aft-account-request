8d6c7015a540616d89c9500b2d6f6f8a24e49563
BurhanKhan <burhankhan1@gmail.com>
2023-07-19 16:05:08 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "btech432023+sandbox11@gmail.com"
+    AccountEmail              = "btech432023+sandbox@gmail.com"
     AccountName               = "Sandbox1"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "btech432023+sandbox11@gmail.com"
-    SSOUserFirstName          = "Network"
+    SSOUserEmail              = "btech432023+sandbox@gmail.com"
+    SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
 
